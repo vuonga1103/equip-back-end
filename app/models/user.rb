4d737fe0a521722906b8173615204e82f17b80cb
vuonga1103 <vuonga1103@yahoo.com>
2020-09-02 18:40:35 -0400

    added gem active_model_serializer, set up relationships for models, and added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :items
+  has_secure_password
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false }
 end
