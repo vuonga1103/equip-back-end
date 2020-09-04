7f02989f944e48a556ea37ef49c53eb7b3640f02
vuonga1103 <vuonga1103@yahoo.com>
2020-09-04 01:17:28 -0400

    created users#create and appropriate route to handle registration of a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,22 @@ class UsersController < ApplicationController
     token = encode_token({user_id: @user.id})
     render json: {user: UserSerializer.new(@user), token: token}
   end
+
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: { user: UserSerializer.new(@user), token: token }
+    else
+      render json: {error: @user.errors.full_messages}
+    end
+
+  end
+
+  private
+  def user_params
+    params.permit(:username, :password, :city, :state, :zip, :email)
+  end
 end
