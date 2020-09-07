09c6759c871cf784ad53173e4052430c899dbe8a
vuonga1103 <vuonga1103@yahoo.com>
2020-09-07 15:47:34 -0400

    added comments

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,20 @@
 class ItemsController < ApplicationController
+ # Before methods update() and create() get called, Application#authorized() is ran; in Application#authorized(), we decode the token passed in via the request's header, find the user associated with the decoded token, and set the instance var of @user to that user. So therefore in update() and create(), we have access to @user which is the user authorized. 
   before_action :authorized, only: [:update, :create]
 
+  # Render out all items that have not been sold to display in frontend / page
   def index
     available_items = Item.all.select { |i| !i.sold }
-
     render json: available_items 
   end
 
+  # To display data for one particular item the user requested
   def show
     item = Item.find_by(id: params[:id])
-
     item ? (render json: item) : (render json: {error: "Item Not Found"})
   end
 
+  # This method gets called when the user updates the sold status of the item. First the user gets authorized through ApplicationController methods. Then we find the item, checks to see if the item belongs to the user, if it does then update the sold column, and return the item
   def update
     @item = Item.find_by(id: params[:id])
 
@@ -22,20 +24,25 @@ class ItemsController < ApplicationController
     else
       render json: { error: "You do no have permission to do this"}
     end
-
   end
 
+  # Gets called when the user submits form to create a new item in frontend. User first gets authorized through ApplicationController methods.
   def create
+    # Then we create a new item, associating it with the @user
     item = @user.items.create(item_params)
 
+    # Then we use Cloudinary::Uploader.upload() method to upload the photo file passed in through params
+
     photo = Cloudinary::Uploader.upload(params[:photo]) 
-    
+    # Next we update the item with the URL we got back from the uploaded photo, we also want to set the sold attribute to false since this is a new item and therefore not yet sold
     item.update(photo: photo['url'], sold: false)
 
+    # Then we render the item
     render json: item
   end
 
   private
+
   def item_params
     params.permit(:name, :description, :condition, :price, :pickup, :shipping, :category, :photo, :latitude, :longitude)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,30 @@
 class UsersController < ApplicationController
+  # Before methods persist() and user_items() get called, Application#authorized() is ran; in Application#authorized(), we decode the token passed in via the request's header, find the user associated with the decoded token, and set the instance var of @user to that user. So therefore in persist() and user_items(), we have access to @user which is the user authorized. 
   before_action :authorized, only: [:persist, :user_items]
 
   def login
     @user = User.find_by(username: params[:username])
 
+    # If the user could be found by username and the password is correct, we create a token ffrom the user's id. We then send back the user and the token
     if @user && @user.authenticate(params[:password])
-
       token = encode_token({user_id: @user.id})
-      
+
+      # Use UserSerializer.new() to ensure that user object returned is formatted with all the attributes we specified in user_serializer
       render json: { user: UserSerializer.new(@user), token: token }
     else
       render json: {error: 'Invalid Username Or Password'}
     end
   end
 
-
   def persist
-    # we have access to @user here thanks to application#authorized and all the helper functions defined in ApplicationController, we use it to encode the user's id into a token, and send back the user and the token
+    # Creating a new token based on the user's id
     token = encode_token({user_id: @user.id})
+
+    # Send back the user and the token
     render json: {user: UserSerializer.new(@user), token: token}
   end
 
-
+  # Creating a new user based on params passed in, if valid, create a token for the user to log them in, render out the user and the token
   def create
     @user = User.create(user_params)
 
@@ -34,6 +37,7 @@ class UsersController < ApplicationController
 
   end
 
+  # Returns all the items belonging to a particular user, whose token was passed in via headers, after running through helper methods in ApplicationController we have access to the @user found
   def user_items
     items = @user.items
     render json: items
